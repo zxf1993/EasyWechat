67881348aafae0d46b1b0c97582d08f9c508b7da
zhangxiufeng <15011211389@163.com>
2017-04-18 17:46:22 +0800

    1

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ define('ROOT_PATH', str_replace('/web', '', str_replace('\\', '/', __DIR__)));
 if (version_compare(PHP_VERSION, '5.3.0', '<')) {
     die('require PHP > 5.3.0 !');
 }
-
+// define('BIND_MODULE', 'Wechat');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG', true);
 
